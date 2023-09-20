f4c821b76af4a481cdbbebf69ff608440c38e029
Shanb98 <shanbasnayake98@gmail.com>
2023-09-20 21:18:41 +0530

    Part 2.1

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes"
+function App() {
+    return (
+        <div>
+            <Header />
+            {notes.map(content => (
+                <Note
+                    key = {content.id}
+                    title = {content.title}
+                    content={content.content}
+                />
+            ))}
+            <Footer />
+        </div>
+    );
+}
+export default App;
